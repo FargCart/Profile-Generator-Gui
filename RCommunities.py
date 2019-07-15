bd6497d6d90153573f7375eea9a3871c51d0197d
Fargo Carterra <fargocarterra@gmail.com>
2019-07-15 16:01:45 -0500

    Delete RCommunities.py

--- RCommunities.py
+++
@@ -1,7 +0,0 @@
-#~/miniconda3/bin/python
-
-import subprocess
-def mybug():
-    # subprocess.run("Rscript -e \"shiny::runApp('NBClust_program',launch.browser=TRUE)\"", shell=True, timeout=60)
-    subprocess.check_output("Rscript -e \"shiny::runApp('NBClust_program',launch.browser=TRUE)\"", shell=True)
-mybug()
